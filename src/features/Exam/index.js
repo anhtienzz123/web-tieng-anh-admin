4765bb2dd33fb445115f12e6189c2c1eae690290
HaoSmall <nhathao00852@gmail.com>
2021-08-14 15:11:29 +0700

    create feature-exam

--- src/features/Exam/index.js
+++ src/features/Exam/index.js
@@ -1,4 +1,5 @@
 import { Spin } from "antd";
+import NotFoundPage from "components/NotFoundPage";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
@@ -7,16 +8,17 @@ import MainPage from "./pages/MainPage";
 Exam.propTypes = {};
 
 function Exam(props) {
-  const { url } = useRouteMatch();
-  const { isLoading } = useSelector((state) => state.exam);
+	const { url } = useRouteMatch();
+	const { isLoading } = useSelector((state) => state.exam);
 
-  return (
-    <Spin spinning={isLoading}>
-      <Switch>
-        <Route exact path={url} component={MainPage} />
-      </Switch>
-    </Spin>
-  );
+	return (
+		<Spin spinning={isLoading}>
+			<Switch>
+				<Route exact path={url} component={MainPage} />
+				<Route component={NotFoundPage} />
+			</Switch>
+		</Spin>
+	);
 }
 
 export default Exam;
